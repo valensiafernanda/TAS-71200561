7df2247d368a3e5b190c2b36ae8445a23902fda1
valensiafernanda <78332934+valensiafernanda@users.noreply.github.com>
2023-01-04 19:42:52 +0700

    Update Patient.java

--- TAS-71200561/PatientDanVirus/src/Patient.java
+++ TAS-71200561/PatientDanVirus/src/Patient.java
@@ -1,3 +1,5 @@
+import java.util.Vector;
+
 public class Patient {
     private int DIE=0;
     private int ALIVE=1;
@@ -20,5 +22,31 @@ public class Patient {
 
     }
 
-    public void 
+    public void DoDie(){
+
+    }
+
+    public int getState(){
+        return m_state;
+    }
+
+    public void setState(int state) {
+        this.m_state = m_state;
+    }
+
+    public int getResistance() {
+        return m_resistance;
+    }
+
+    public void setResistance(int resistance) {
+        this.m_resistance = m_resistance;
+    }
+
+    public Vector<Virus> getVirusList(){
+
+    }
+
+    public void setVirusList(Vector<Virus>){
+
+    }
 }
